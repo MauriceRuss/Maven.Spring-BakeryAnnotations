10c33784e43ff779c39d1ee81859b341b6d6d147
Mo Russ <mauriceruss302@gmail.com>
2020-03-25 23:53:27 -0400

    muffintests passed

--- src/main/java/com/zipcodewilmington/bakery/controllers/BakerController.java
+++ src/main/java/com/zipcodewilmington/bakery/controllers/BakerController.java
@@ -16,27 +16,27 @@ import javax.persistence.Entity;
 public class BakerController {
 
     private BakerService service;
-
+    @Autowired
     public BakerController(BakerService service) {
         this.service = service;
     }
-    @GetMapping("/bakers")
+    @GetMapping("/bakers/")
     public ResponseEntity<Iterable<Baker>> index() {
         return new ResponseEntity<>(service.index(), HttpStatus.OK);
     }
-    @GetMapping("/baker/{id")
+    @GetMapping("/bakers/{id}")
     public ResponseEntity<Baker> show(@PathVariable Long id) {
         return new ResponseEntity<>(service.show(id), HttpStatus.OK);
     }
-    @PostMapping("/bakers")
+    @PostMapping("/bakers/")
     public ResponseEntity<Baker> create(@RequestBody Baker baker) {
         return new ResponseEntity<>(service.create(baker), HttpStatus.CREATED);
     }
-    @PutMapping("/bakers/{id")
+    @PutMapping("/bakers/{id}")
     public ResponseEntity<Baker> update(@PathVariable Long id, @RequestBody Baker baker) {
         return new ResponseEntity<>(service.update(id, baker), HttpStatus.OK);
     }
-    @DeleteMapping("/bakers/{id")
+    @DeleteMapping("/bakers/{id}")
     public ResponseEntity<Boolean> destroy(@PathVariable Long id) {
         return new ResponseEntity<>(service.delete(id), HttpStatus.OK);
     }
